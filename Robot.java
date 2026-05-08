d01871721a8aa815619d43e65628f2bb11381247
dorukarasyildiz-lgtm <dorukarasyildiz@icloud.com>
2026-05-08 20:18:48 +0300

    Robot.java

--- Robot.java
+++ Robot.java
@@ -1,1 +1,17 @@
+package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.subsystems.Arm;
+
+public class RobotContainer {
+  public RobotContainer() {
+    configureBindings();
+  }
+Arm arm = new Arm();
+  private void configureBindings() {}
+
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
+}
